1604ccd1988dc6e807687831f008f38dde2fb404
Daniel Elebash <delebash@users.noreply.github.com>
2022-10-16 17:36:39 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+/* Npm module created from https://codepen.io/roblabs/pen/zJjPzX */
+class MapboxGLButtonControl {
+    constructor({
+                    className = "",
+                    title = "",
+                    eventHandler = evtHndlr
+                }) {
+        this._className = className;
+        this._title = title;
+        this._eventHandler = eventHandler;
+    }
+
+    onAdd(map) {
+        this._btn = document.createElement("button");
+        this._btn.className = "mapboxgl-ctrl-icon" + " " + this._className;
+        this._btn.type = "button";
+        this._btn.title = this._title;
+        this._btn.onclick = this._eventHandler;
+
+        this._container = document.createElement("div");
+        this._container.className = "mapboxgl-ctrl-group mapboxgl-ctrl";
+        this._container.appendChild(this._btn);
+
+        return this._container;
+    }
+
+    onRemove() {
+        this._container.parentNode.removeChild(this._container);
+        this._map = undefined;
+    }
+}
+
+export {MapboxGLButtonControl}
